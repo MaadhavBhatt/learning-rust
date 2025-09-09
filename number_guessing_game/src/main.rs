9e1c62ba093ad898385098653f779dfab06aa517
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-09-09 08:31:23 +0000

    Track valid and invalid attempts in the number guessing game Terminate program when max guess limit is reached

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -4,10 +4,15 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+const MAX_VALID_ATTEMPTS: u8 = 10;
+const MAX_INVALID_ATTEMPTS: u8 = 3;
+
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    let mut valid_attempts: u8 = 0;
+    let mut invalid_attempts: u8 = 0;
 
     loop {
         println!("Please input your guess.");
@@ -19,9 +24,13 @@ fn main() {
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+            Ok(num) => {
+                increment_valid_attempts(&mut valid_attempts);
+                num
+            }
             Err(_) => {
-                println!("Please enter a valid number!");
+                println!("Please enter a valid integer between 1 and 100 (inclusive).");
+                increment_invalid_attempts(&mut invalid_attempts);
                 continue;
             }
         };
@@ -37,4 +46,24 @@ fn main() {
             }
         }
     }
+}
+
+fn increment_attempts(attempts: &mut u8, max_attemps: u8, attempt_type: &str) {
+    if *attempts < max_attemps - 1 {
+        *attempts += 1;
+    } else {
+        println!(
+            "You've reached the maximum number of {} attempts ({}). Game over!\nThanks for playing!",
+            attempt_type, max_attemps
+        );
+        std::process::exit(0);
+    }
+}
+
+fn increment_valid_attempts(attempts: &mut u8) {
+    increment_attempts(attempts, MAX_VALID_ATTEMPTS, "valid");
+}
+
+fn increment_invalid_attempts(attempts: &mut u8) {
+    increment_attempts(attempts, MAX_INVALID_ATTEMPTS, "invalid");
 }
